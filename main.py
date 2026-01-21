3ad01864520eff812dc2ed9f0a8d3dce7d63e91f
Dominik <101473446+b14ucky@users.noreply.github.com>
2026-01-21 20:32:44 +0100

    fix: prevent forest generation when changing parameters not related with forest

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from enum import Enum
 import streamlit as st
 import matplotlib.cm as cm
+from dataclasses import asdict
 from PIL import Image, ImageDraw
 from pyforest import VegetationType
 from utils import (
@@ -204,20 +205,48 @@ with right:
 
     trees_left, trees_rigth = st.columns(2)
 
+    if "forest_config" not in st.session_state:
+        st.session_state.forest_config = PyForestConfig(
+            width=width,
+            height=height,
+        )
+
+    def update_forest(key: str | None = None) -> None:
+        forest_config_dict = asdict(st.session_state.forest_config)
+
+        if key is not None:
+            forest_config_dict[key] = st.session_state[key]
+            st.session_state.forest_config = PyForestConfig(**forest_config_dict)
+
+        # if "heightmap" in st.session_state:
+        st.session_state.forest_map = generate_forest_adapted_to_terrain(
+            config=st.session_state.forest_config,
+            heightmap=st.session_state.heightmap,
+        )
+
     with trees_left:
         initial_trees = st.number_input(
             "Number of initial trees:",
             min_value=0,
             value=3,
+            key="initial_trees",
+            on_change=update_forest,
+            args=("initial_trees",),
         )
         space_between_trees = st.number_input(
             "Space between trees:",
             min_value=5,
+            key="space_between_trees",
+            on_change=update_forest,
+            args=("space_between_trees",),
         )
         n_iterations = st.number_input(
             "Number of iterations:",
             min_value=1,
             value=3,
+            key="n_iterations",
+            on_change=update_forest,
+            args=("n_iterations",),
         )
 
     with trees_rigth:
@@ -225,6 +254,9 @@ with right:
             "Seeding radius:",
             min_value=0,
             value=15,
+            key="seed_radius",
+            on_change=update_forest,
+            args=("seed_radius",),
         )
         seed_strength = st.number_input(
             "Seed strength:",
@@ -232,6 +264,9 @@ with right:
             max_value=1.00,
             value=0.05,
             step=0.01,
+            key="seed_strength",
+            on_change=update_forest,
+            args=("seed_strength",),
         )
         seed_decay_rate = st.number_input(
             "Seed decay rate:",
@@ -239,18 +274,10 @@ with right:
             max_value=1.00,
             value=0.2,
             step=0.01,
+            key="seed_decay_rate",
+            on_change=update_forest,
+            args=("seed_decay_rate",),
         )
-
-    forest_config = PyForestConfig(
-        width=width,
-        height=height,
-        initial_trees=initial_trees,
-        seed_radius=seed_radius,
-        seed_strength=seed_strength,
-        seed_decay_rate=seed_decay_rate,
-        n_iterations=n_iterations,
-        space_between_trees=space_between_trees,
-    )
 
     # water settings section
     st.divider()
@@ -316,22 +343,28 @@ with right:
 
 with left:
     with st.spinner("Generating..."):
-        heightmap = generate_heightmap(
+        st.session_state.heightmap = generate_heightmap(
             config=config, mountains=mountains, terrain_amplifier=0.7
         )
 
-        heightmap = heightmap * final_water_position if water_on else heightmap + 0.2
-
-        forest_map = generate_forest_adapted_to_terrain(
-            config=forest_config, heightmap=heightmap
+        st.session_state.heightmap = (
+            st.session_state.heightmap * final_water_position
+            if water_on
+            else st.session_state.heightmap + 0.2
         )
 
-    colored_map = cm.terrain(heightmap)  # type: ignore
+        if "forest_map" not in st.session_state:
+            st.session_state.forest_map = generate_forest_adapted_to_terrain(
+                config=st.session_state.forest_config,
+                heightmap=st.session_state.heightmap,
+            )
+
+    colored_map = cm.terrain(st.session_state.heightmap)  # type: ignore
     img_array = (colored_map[:, :, :3] * 255).astype("uint8")
     image = Image.fromarray(img_array)
     draw = ImageDraw.Draw(image)
 
-    y_trees, x_trees = np.where(forest_map == VegetationType.TREE)
+    y_trees, x_trees = np.where(st.session_state.forest_map == VegetationType.TREE)
 
     tree_size = 4
     for x, y in zip(x_trees, y_trees):
@@ -346,8 +379,9 @@ with left:
     with st.container(horizontal_alignment="center"):
         col_left, col_right = st.columns(2)
         with col_left:
-            if st.button("Refresh", width="stretch"):
-                pass
+            if st.button("Update Forest", width="stretch"):
+                update_forest()
+                st.rerun()
 
         with col_right:
             if st.button("Export to Unreal", width="stretch"):
@@ -359,8 +393,8 @@ with left:
                     Scale=100.0,
                     ZMultiplier=7000.0,
                     UVScale=1.0,
-                    Heightmap=heightmap.reshape(-1).tolist(),
-                    VegetationMap=forest_map.reshape(-1).tolist(),
+                    Heightmap=st.session_state.heightmap.reshape(-1).tolist(),
+                    VegetationMap=st.session_state.forest_map.reshape(-1).tolist(),
                     bWaterOn=water_on,
                     bFogOn=fog_on,
                     FogHeightOffset=height_fog_offset.value,
